7e0ee8548140cd22dca342bb34519cb0ed5e0b3e
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-02-24 16:46:06 +0100

    Starting to implement GL33.

--- src/Graphics/Luminance/Driver/GL33.hs
+++ src/Graphics/Luminance/Driver/GL33.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 -----------------------------------------------------------------------------
 -- |
 -- Copyright   : (C) 2015, 2016 Dimitri Sabadie
@@ -11,3 +13,9 @@
 module Graphics.Luminance.Driver.GL33 (
     -- *
   ) where
+
+import qualified Graphics.Luminance.Driver.GL33.Buffer as GL33
+import Graphics.Luminance.BufferDriver ( BufferDriver )
+
+-- |OpenGL 3.3 luminance interpreter.
+newtype GL33 m a = GL33 { runGL33 :: m a } deriving (Applicative,Functor,Monad)
