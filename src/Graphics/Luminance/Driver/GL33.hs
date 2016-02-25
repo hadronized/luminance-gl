b9754bc95f570cada487976020e396f651b928b1
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-02-25 18:16:53 +0100

    runGL.

--- src/Graphics/Luminance/Driver/GL33.hs
+++ src/Graphics/Luminance/Driver/GL33.hs
@@ -11,28 +11,26 @@
 -- Portability : portable
 -----------------------------------------------------------------------------
 
-module Graphics.Luminance.Driver.GL33 (
-    -- *
-  ) where
+module Graphics.Luminance.Driver.GL33 where
 
-import Control.Monad.IO.Class ( MonadIO )
+import Control.Monad.IO.Class ( MonadIO(..) )
 import Control.Monad.Base ( MonadBase )
 import Control.Monad.Trans ( lift )
-import Control.Monad.Trans.Either ( EitherT )
-import Control.Monad.Trans.Resource ( MonadResource(..), MonadThrow, ResourceT )
+import Control.Monad.Trans.Either ( EitherT(..) )
+import Control.Monad.Trans.Resource ( MonadResource(..), MonadThrow, ResourceT, runResourceT )
 import qualified Graphics.Luminance.Driver.GL33.Buffer as GL33
 import Graphics.Luminance.BufferDriver ( BufferDriver(..) )
 
 -- |OpenGL 3.3 luminance interpreter.
-newtype GL33 a = GL33 { runGL33 :: EitherT GL33Error (ResourceT IO) a } deriving (Applicative,Functor,Monad,MonadIO,MonadBase IO,MonadThrow)
+newtype GL a = GL { runGL' :: EitherT GLError (ResourceT IO) a } deriving (Applicative,Functor,Monad,MonadIO,MonadBase IO,MonadThrow)
 
-instance MonadResource GL33 where
-  liftResourceT = GL33 . lift
+instance MonadResource GL where
+  liftResourceT = GL . lift
 
-instance BufferDriver GL33 where
-  type BuildBuffer GL33 = GL33.BuildBuffer
-  type Buffer GL33 = GL33.Buffer
-  type BufferRW GL33 = GL33.BufferRW
+instance BufferDriver GL where
+  type BuildBuffer GL = GL33.BuildBuffer
+  type Buffer GL = GL33.Buffer
+  type BufferRW GL = GL33.BufferRW
   createRegion = GL33.createRegion
   createBuffer = GL33.createBuffer
   readWhole = GL33.readWhole
@@ -44,4 +42,7 @@ instance BufferDriver GL33 where
   writeAt' = GL33.writeAt'
 
 -- |OpenGL 3.3 error type.
-data GL33Error
+data GLError
+
+runGL :: (MonadIO m) => GL a -> m (Either GLError a)
+runGL = liftIO . runResourceT . runEitherT . runGL'
